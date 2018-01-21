524d1b83354cf625757a7f3a9c37f5f13a4a38ca
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-01-21 15:46:52 +0530

    Create arith.c

---
+++ arith.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int N,A,D,t,k,i;
+	scanf("%d%d%d",&N,&A,&D);
+	for (i=0;i<=A;i++)
+	{
+		k=0;
+	t=N+D;
+	k=t+N;
+N=t;
+	}
+	printf("aritnmetic progression is %d",k);
+	return 0;
+}
