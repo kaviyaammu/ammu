1067db7b225bc32673a93e508401f0421f67a669
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-01-21 16:54:09 +0530

    Create hour.c

---
+++ hour.c
@@ -0,0 +1,11 @@
+    #include <stdio.h>
+     
+    int main(void) 
+    {
+    float hour1,hour2,hour;
+    scanf("%f%f",&hour1,&hour2);
+    hour=hour1-hour2;
+    printf("%f",hour);
+    	return 0;
+    }
+     
