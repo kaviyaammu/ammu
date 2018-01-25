ca2e87dba52c7822da035a052e13715a56504df7
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-01-25 11:09:32 +0000

    Create arrfind.c

---
+++ arrfind.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,t,k,a[10];
+	scanf("%d",&t);
+	scanf("%d",&k);
+	for(i=0;i<=t;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("%d",a[k]);
+	
+	return 0;
+}
