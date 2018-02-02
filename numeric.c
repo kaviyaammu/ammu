806ce24d97257952a7dd64ff0fe6405eb90d9e0e
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-02-02 14:45:26 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char a;
+	scanf("%s",&a);
+	if((a>='0')&&(a<='9'))
+	{
+	printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
