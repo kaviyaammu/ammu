5c23f8a3838d0062ca06ae5d8986fbd4838cba23
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-02-08 14:30:44 +0530

    Create special.c

---
+++ special.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	char t[50];
+	int j,flag=0;
+	gets(t);
+	for(j=0;t[j]!='\0';j++)
+	{
+		if((t[j]>='!')&&(t[j]<='@'))
+		{
+			flag++;
+		}
+	}
+	printf("%d",flag);
+	return 0;
+}
