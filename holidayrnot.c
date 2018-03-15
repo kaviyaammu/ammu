37b365e1910042fca518a3a0711498f79b8659a2
kaviyaammu <31505161+kaviyaammu@users.noreply.github.com>
2018-03-15 16:46:16 +0530

    Create holidayrnot.c

---
+++ holidayrnot.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char t[50];
+    scanf("%s",&t);
+    if(t[0]=='s' || t[0]=='S')
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+    
+   
+}
